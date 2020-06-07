d79fb62c34a09b71e93239717f599a2e371e255b
zkytech <zhangkunyuan@hotmail.com>
2020-06-07 17:15:28 +0800

    完成15

---
+++ webpack4/15html压缩/webpack.config.js
@@ -0,0 +1,27 @@
+const {resolve} = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/built.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: {
+                // 移除空格
+                collapseWhitespace:true,
+                // 移除注释
+                removeComments:true
+            }
+            
+        })
+    ],
+    mode: 'production'
+
+}
